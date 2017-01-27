67f2fa3c46a331e367c8a5e5c38089ab03fc4cda
Justin <justinlove1987@gmail.com>
2017-01-26 21:12:19 -0600

    Revert "Updated the version of typescript."
    This reverts commit 9b7654afee89e5883a02f54f9b16abae559278a9.

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,10 @@ import * as Puzzles from './Puzzles';
 import { GridSize } from './GridSize';
 
 Program.main([
-    { values: Puzzles.Puzzle9x9.DevilPuzzle, size: GridSize.Default, label: 'Devil' }
-    //{ values: Puzzles.Puzzle9x9.EmptyPuzzle, size: GridSize.Default, label: 'Empty' },
-    //{ values: Puzzles.Puzzle9x9.EasyPuzzle, size: GridSize.Default, label: 'Easy' },
-    //{ values: Puzzles.Puzzle9x9.MediumPuzzle, size: GridSize.Default, label: 'Medium' },
-    //{ values: Puzzles.Puzzle9x9.HardPuzzle, size: GridSize.Default, label: 'Hard' },
+    { values: Puzzles.Puzzle9x9.EmptyPuzzle, size: GridSize.Default, label: 'Empty' },
+    { values: Puzzles.Puzzle9x9.EasyPuzzle, size: GridSize.Default, label: 'Easy' },
+    { values: Puzzles.Puzzle9x9.MediumPuzzle, size: GridSize.Default, label: 'Medium' },
+    { values: Puzzles.Puzzle9x9.HardPuzzle, size: GridSize.Default, label: 'Hard' },
     
-    //{ values: Puzzles.Puzzle16x16.UnknownDifficultyPuzzle, size: new GridSize(4, 4), label: 'Unknown Difficulty' }
+    { values: Puzzles.Puzzle16x16.UnknownDifficultyPuzzle, size: new GridSize(4, 4), label: 'Unknown Difficulty' }
 ]);
